29dd955ee029798132392addfd38ac55b35b8b44
PedroDLuevano <pluevano497@gmail.com>
2026-08-12 15:59:01 -0600

    :sparkles: Nueva ruta user e instalacion de nodemon para actualizacion continua

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+// Recuerda dejar al final la ruta genera a la que se redigirá el usuario
+
+app.use('/user',(req,res,next)=>{
+    res.status(200).json({
+        message:"hello this is the user request"
+    });
+});
+
+
 app.use('/',(req,res,next)=>{
     res.status(200).json({
         message:"hello world"
     });
 });
-
 module.exports=app;
